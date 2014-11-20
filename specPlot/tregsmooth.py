516d675dfd541047824e5d9432c48d03f7fda438
Alex J. Grede <agrede@gmail.com>
2014-11-20 08:39:12 +0000

    Removed sparse matricies for now to get it working

--- specPlot/tregsmooth.py
+++ specPlot/tregsmooth.py
@@ -1,6 +1,7 @@
 import numpy as np
 from scipy.optimize import minimize
-from scipy.sparse import identity, diags
+# from scipy.sparse import identity, diags
+from numpy import identity, diag
 from scipy.interpolate import interp1d
 
 
@@ -9,28 +10,29 @@ def ddmat(x, d):
     if (d == 0):
         D = identity(m)
     else:
-        dx = x[d:(m - 1)] - x[0:(m - d - 1)]
-        V = diags(1 / dx)
-        D = d * np.dot(V, ddmat(x, d - 1))
+        dx = x[d:] - x[0:(m - d)]
+        V = diag(1 / dx)
+        D = d * np.dot(V, np.diff(ddmat(x, d - 1), axis=0))
     return D
 
 
 def rgdtsmcore(x, y, d, lam, xh):
     N = x.size
-    Nh = x.size
+    Nh = xh.size
     idx = interp1d(xh, np.arange(Nh), kind='nearest')
-    M = identity(Nh)[idx(x), :]
+    M = identity(Nh)[idx(x).astype(int), :]
     D = ddmat(xh, d)
     W = identity(N)
     U = identity(Nh-d)
-    delta = np.trace(np.dot(D.conj().transpose(), D)) / Nh**(2 + d)
+    delta = np.trace(np.dot(D.conj().transpose(), D)) / np.power(Nh, (2 + d))
     A = (np.dot(M.conj().transpose(), np.dot(W, M))
          + lam/delta * np.dot(D.conj().transpose(), np.dot(U, D)))
     yh = np.linalg.solve(A,
                          np.dot(M.conj().transpose(), np.dot(W, y)))
     H = np.dot(M, np.linalg.solve(A, np.dot(M.conj().transpose(), W)))
     R = (np.dot(M, yh) - y)
-    v = np.dot(R.conj().transpose(), R) / (N * (1 - np.trace(H) / N)**2)
+    v = np.dot(R.conj().transpose(), R) / np.power(N * (1 - np.trace(H) / N),
+                                                   2)
     return (yh, v)
 
 
@@ -39,6 +41,6 @@ def regdatasmooth(x, y, xh, d=2):
     guess = 0
     f = lambda lam: rgdtsmcore(x, y, d, lam, xh)[1]
     lam = minimize(f, guess, method='Nelder-Mead', tol=1e-6,
-                   options={'maxiter': maxiter, 'disp': True})
+                   options={'maxiter': maxiter, 'disp': True}).x[0]
     yh = rgdtsmcore(x, y, d, lam, xh)[0]
     return yh
