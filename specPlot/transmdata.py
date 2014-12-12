def2727ba76b01e0a1ae4dafb83c4b91d6efeeb8
Alex J. Grede <agrede@gmail.com>
2014-12-12 07:38:37 +0000

    Bug fixes

--- specPlot/transmdata.py
+++ specPlot/transmdata.py
@@ -11,15 +11,15 @@ import numpy as np
 
 def rawdata(paths, mult, skip=1):
     for idx, path in enumerate(paths):
-        if (type(mult) is np.ndarray()):
+        if (type(mult) is np.ndarray):
             m = mult[idx]
         else:
             m = mult
-        tmp = np.getfromtxt(path, delimiter="\t", skip_header=skip)
+        tmp = np.genfromtxt(path, delimiter="\t", skip_header=skip)
         if (idx == 0):
             lam = tmp[:, 0]*1e-9
-            amp = np.zeros((lam.size, paths.size), float)
-            phase = np.zeros((lam.size, paths.size), float)
+            amp = np.zeros((lam.size, len(paths)), float)
+            phase = np.zeros((lam.size, len(paths)), float)
         amp[:, idx] = tmp[:, 1]*m
         phase[:, idx] = np.radians(tmp[:, 2])
     return (lam, amp, phase)
